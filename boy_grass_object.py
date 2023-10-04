088f73305a658db013f1511be30f5795ed5b4a57
icktae <taechil77@naver.com>
2023-10-04 19:57:21 +0900

    reformat

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -1,18 +1,21 @@
 from pico2d import *
 import random
 
+
 # Game object class here
 
 
 class Grass:
     def __init__(self):
         self.image = load_image('grass.png')
+
     def draw(self):
         self.image.draw(400, 30)
+
     def update(self): pass
 
 
-class Boy :
+class Boy:
     def __init__(self):
         self.x, self.y = random.randint(100, 700), 90
         self.frame = random.randint(0, 7)
@@ -25,23 +28,25 @@ class Boy :
     def draw(self):
         self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
 
-class Ball :
+
+class Ball:
     def __init__(self):
         self.small_image = load_image('ball21x21.png')
         self.big_image = load_image('ball41x41.png')
-        self.x, self.y = random.randint(0,800), 600
+        self.x, self.y = random.randint(0, 800), 600
         self.speed = random.randint(3, 15)
-        self.ball_sizle = random.randint(0,1)
+        self.ball_sizle = random.randint(0, 1)
 
     def update(self):
-        if self.y >= 60 :
+        if self.y >= 60:
             self.y -= self.speed
 
     def draw(self):
-        if self.ball_sizle == 0 :
+        if self.ball_sizle == 0:
             self.small_image.draw(self.x, self.y)
-        else :
+        else:
             self.big_image.draw(self.x, self.y)
+
 
 def handle_events():
     global running
@@ -53,7 +58,7 @@ def handle_events():
             running = False
 
 
-def reset_world() :
+def reset_world():
     global running
     global grass
     global boy
@@ -72,19 +77,20 @@ def reset_world() :
 
     world += team
 
-def update_world() :
-  for o in world :
-      o.update()
 
-  for ball in balls :
-      ball.update()
+def update_world():
+    for o in world:
+        o.update()
+
+    for ball in balls:
+        ball.update()
 
 
-def render_world() :
+def render_world():
     clear_canvas()
-    for o in world :
+    for o in world:
         o.draw()
-    for ball in balls :
+    for ball in balls:
         ball.draw()
     update_canvas()
 
@@ -94,14 +100,12 @@ open_canvas()
 # initialization code
 reset_world()
 
-
 # game main loop code
-while running :
+while running:
     handle_events()
     update_world()
     render_world()
     delay(0.05)
-
 
 # finalization code
 
